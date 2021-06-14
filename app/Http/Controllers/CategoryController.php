8896fc9039a284e8c04f322127818342a2d549f9
KabirLiaquat <kabir.liaquat@gmail.com>
2021-06-15 01:21:57 +0500

    Eloquent ORM Read Data

--- app/Http/Controllers/CategoryController.php
+++ app/Http/Controllers/CategoryController.php
@@ -12,7 +12,8 @@ class CategoryController extends Controller
 {
     public function AllCat()
     {
-      return view('admin.category.index');
+      $categories = Category::latest()->get();
+      return view('admin.category.index', compact('categories'));
     }
 
     public function AddCat(Request $request)
@@ -25,21 +26,21 @@ class CategoryController extends Controller
         'category_name.max' => 'Category Less than 255 Charachters',
       ]);
 
-      // Category::insert([
-      //   'category_name' => $request->category_name,
-      //   'user_id' => Auth::user()->id,
-      //   'created_at' => Carbon::now()
-      // ]);
+      Category::insert([
+        'category_name' => $request->category_name,
+        'user_id' => Auth::user()->id,
+        'created_at' => Carbon::now()
+      ]);
 
       // $category = new Category();
       // $category->category_name = $request->category_name;
       // $category->user_id = Auth::user()->id;
       // $category->save();
 
-      $data = array();
-      $data['category_name'] = $request->category_name;
-      $data['user_id'] = Auth::user()->id;
-      DB::table('categories')->insert($data);
+      // $data = array();
+      // $data['category_name'] = $request->category_name;
+      // $data['user_id'] = Auth::user()->id;
+      // DB::table('categories')->insert($data);
 
       return redirect()->back()->with('success', 'Category Inserted Successfully');
 
