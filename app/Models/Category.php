200aa0d2aad30168430c7118887afd8bb6ecdced
KabirLiaquat <kabir.liaquat@gmail.com>
2021-06-15 03:50:30 +0500

    Query Builder Join Table

--- app/Models/Category.php
+++ app/Models/Category.php
@@ -15,8 +15,8 @@ class Category extends Model
       'password',
     ];
 
-    public function user()
-    {
-      return $this->hasOne(User::class, 'id', 'user_id');
-    }
+    // public function user()
+    // {
+    //   return $this->hasOne(User::class, 'id', 'user_id');
+    // }
 }
