de25ff916e69e17abc920251807e2e83887b7179
KabirLiaquat <kabir.liaquat@gmail.com>
2021-06-15 05:03:30 +0500

    Query Builder Edit & Update

--- app/Http/Controllers/CategoryController.php
+++ app/Http/Controllers/CategoryController.php
@@ -58,21 +58,28 @@ class CategoryController extends Controller
 
     public function Edit($id)
     {
-      $categories = Category::find($id);
+      // $categories = Category::find($id);
+      $categories = DB::table('categories')->where('id', $id)->first();
       return view('admin.category.edit', compact('categories'));
     }
 
     public function Update(Request $request ,$id)
     {
-      $update = Category::find($id)->update([
-        'category_name' => $request->category_name,
-        'user_id' => Auth::user()->id,
-      ]);
+
+      // $update = Category::find($id)->update([
+      //   'category_name' => $request->category_name,
+      //   'user_id' => Auth::user()->id,
+      // ]);
 
       // $category = Category::findOrFail($id);
       // $category->category_name = $request->category_name;
       // $category->user_id = Auth::user()->id;
       // $category->save();
+
+      $data = array();
+      $data['category_name'] = $request->category_name;
+      $data['user_id'] = Auth::user()->id;
+      DB::table('categories')->where('id', $id)->update($data);
 
       return redirect()->route('all.category')->with('success', 'Category Updated Successfully');
     }
